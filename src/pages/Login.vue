5d76324f43a5a04d8d42c070da004ec1d7eac3fc
Lucas Gladding <l.gladding@vehikl.com>
2020-11-17 17:56:41 -0500

    Added Login page and tests.

---
+++ src/pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login-page">
+    <div>
+      <input ref="username-field" v-model="username" />
+    </div>
+    <div>
+      <input ref="password-field" type="password" v-model="password" />
+    </div>
+    <div>
+      <button ref="login-button" @click="login">Login</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+const Login = Vue.extend({
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      const { username, password } = this;
+      this.$emit('login', { username, password });
+    },
+  },
+});
+
+export default Login;
+</script>
---
+++ tests/unit/pages/Login.spec.ts
@@ -0,0 +1,17 @@
+import { mount } from '@vue/test-utils';
+
+import Login from '@/pages/Login.vue';
+
+describe('Login', () => {
+  it('sends the login event with the form data', () => {
+    const data = {
+      username: 'lucas',
+      password: 'password',
+    };
+    const wrapper = mount(Login);
+    wrapper.findComponent({ ref: 'username-field' }).setValue(data.username);
+    wrapper.findComponent({ ref: 'password-field' }).setValue(data.password);
+    wrapper.findComponent({ ref: 'login-button' }).trigger('click');
+    expect(wrapper.emitted('login')![0][0]).toEqual(data);
+  });
+});
